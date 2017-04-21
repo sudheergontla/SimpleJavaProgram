06b3b38275183912de379ed4fbec240c8cf0811c
sudheergontla <sudheerg@astutebs.com>
2017-04-21 11:03:06 +0530

    it will display A value

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package org.astute; /*package creation must be reverse of domain name i.e."astute.org" this is domain name */ 
+import java.lang.*; /*usually importing java.lang package is optional.*/
+
+public class Demo /*This is the class name must be public. but it is recommendable but not compulsory */
+{
+static int a=10;
+public static void main(String [] args){
+System.out.println("a value"+a);
+}
+}
